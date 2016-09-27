b608b3716992ca067383136a4c7640887b7af155
Benjamin Petit <benjamin.petit@polytechnique.edu>
2016-09-27 16:41:46 +0200

    Added core.py, with more payment checking functions

--- Core.py
+++ Core.py
@@ -19,32 +19,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import cryptography.hazmat.primitives.asymmetric.rsa as crypt
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from threading import Thread
-import socket, json, random, string, hashlib, time, sys
+import socket, json, hashlib, time, sys
 
 reload(sys)
 sys.setdefaultencoding("ISO-8859-1")
-
-def randomWord(length):
-   return ''.join(random.choice(string.lowercase) for i in range(length))
-
-def deleteMultipleEntries(l):
-    cleanList = []
-    for e in l:
-        if e not in cleanList:
-            cleanList.append(e)
-    return cleanList
             
 class Peer:
     publicKey = None
     privateKey = None    
     port = None
-    friendPeers = None #friendPeers is a SET of tuples (target type), not a list. 
+    friendPeers = None #friendPeers is a list of tuples (target-type)
     listener = None
-    entries = set([])
+    entries = {}
     
     def __init__(self, port, friendPeers):
         self.privateKey = crypt.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
@@ -55,31 +44,50 @@ class Peer:
         return
     
     def makeFriends(self):
-        for friend in self.friendPeers:
-            fP = list(self.friendPeers)            
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)            
-            try:            
-                sock.connect(friend)
-                sock.send(json.dumps(['REQUEST_FRIENDS']))
-                newFriends = json.loads(sock.recv(2500))
-                fP = fP + [tuple(e) for e in newFriends]
-            except:
-                fP.remove(friend)
-            self.friendPeers = deleteMultipleEntries(fP)
+        fP = list(self.friendPeers) 
+        for friend in fP:
+            self.sendRequest(['REQUEST_FRIENDS'], friend)
+        return True
+        
+    def addFriends(self, friendList):
+        for friend in friendList:
+            if friend not in self.friendPeers:
+                self.friendPeers.append(friend)
         return True
     
     def sendRequest(self, request, target):
-        #Sends a request after authentication.
+        #Sends a signed request..
         #target is a tuple containing target and port.
         signer = self.privateKey.signer(padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
         signer.update(json.dumps(request))
         digitalSignature = signer.finalize()
-        data = request + [self.publicKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo), unicode(digitalSignature)]
+        data = request + [self.publicKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo), unicode(digitalSignature), time.time(), unicode(hashlib.sha512(json.dumps(request)).hexdigest())]
         data = json.dumps(data)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(target)
         sock.send(data)
-        return
+        
+    def sendRequestOnSocket(self, request, sock):
+        #Sends a signed request..
+        #target is a tuple containing target and port.
+        signer = self.privateKey.signer(padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
+        signer.update(json.dumps(request))
+        digitalSignature = signer.finalize()
+        data = request + [self.publicKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo), unicode(digitalSignature), time.time(), unicode(hashlib.sha512(json.dumps(request)).hexdigest())]
+        data = json.dumps(data)
+        sock.send(data)
+        
+    def writeRequest(self, request, originPublicKey, digitalSignature, timestamp, requestHash):
+        print "writeRequest"        
+        self.entries[requestHash] = [request, originPublicKey, digitalSignature, timestamp, requestHash]
+        print "request written"        
+        return True
+    
+    def spreadEntry(self, dataHash):       
+        for target in self.friendPeers:
+            print "spreadEntry"
+            self.sendRequest(['AWARE_REQUEST', dataHash])
+        return True
 
     def receive(self):
         self.listener = PeerListener(self)
@@ -89,7 +97,7 @@ class Peer:
 class PeerListener(Thread):
     serverSocket = None
     parentPeer = None
-    clientSockets = set([])
+    #clientSockets = set([])
     
     def __init__(self, parentPeer):
         Thread.__init__(self)
@@ -97,61 +105,102 @@ class PeerListener(Thread):
         self.parentPeer = parentPeer
         self.serverSocket.bind(('localhost', self.parentPeer.port))
         
-    def sendFriends(self, sock):
-        sock.send(json.dumps(self.parentPeer.friendPeers))
+    def sendFriends(self, clientSocket):
+        self.parentPeer.sendRequestOnSocket(['FRIENDS_LIST',json.dumps(self.parentPeer.friendPeers)], clientSocket)
         return True
         
-    def checkRequest(self, request, digitalSignature, requesterPublicKey):
+    def checkRequest(self, request, originPublicKey, digitalSignature, timestamp, requestHash):
         try:
-            verifier = requesterPublicKey.verifier(bytes(digitalSignature),padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
+            verifier = originPublicKey.verifier(bytes(digitalSignature),padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
             verifier.update(json.dumps(request))
             verifier.verify()
+            if timestamp > time.time():
+                raise(Exception)
+            if unicode(requestHash) != unicode(hashlib.sha512(json.dumps(request)).hexdigest()):
+                raise(Exception)
         except:
             print "Request is invalid ; authentication failed."
             return False
-        print "Client properly authenticated the request. Processing..."
+        print "Request is correct. Processing..."
         return True
         
-    def secureRequest(self, data):
-        #Receive a request and authenticate its emitter.
-        requesterPublicKey = serialization.load_pem_public_key(str(data[-2]), backend=default_backend())
-        request = data[:-2]
-        digitalSignature = data[-1]
-        if self.checkRequest(request, digitalSignature, requesterPublicKey):
-            self.processRequest(request)        
-            return True
-        else:
+    def checkTransaction(self, request, originPublicKey):
+        amount = request[1]
+        transactionList = request[2]
+        #Check whether transactionList is a list of valid transactions
+        fullTransactionList = []
+        for transactionHash in transactionList:
+            transaction = self.parentPeer.entries.get(transactionHash) 
+            if not transaction:
+                return False
+            fullTransactionList.append(transaction)
+        #Checks whether the payer has enough money to pay.
+        if sum([e[1] for e in fullTransactionList]) != amount:
             return False
-        
-    def processRequest(self, request):
-        requestBlock = hashlib.sha512(json.dumps(request)).hexdigest()
-        print json.dumps(request), requestBlock, len(requestBlock)
+        #Checks whether transactions are owned by payer.
+        for e in fullTransactionList:
+            if e[3] != originPublicKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo):       
+                return False
+        #NEED TO ADD A CHECK WHETHER TRANSACTION IS ALREADY SPEND OR NOT // SPENT TRANSACTIONS DICT?
+        return True
+
+    def processRequest(self, clientSocket, request, originPublicKey, digitalSignature, timestamp, requestHash):
         if request[0] == 'PAY':
-            newBlock = [request, hashlib.sha512(json.dumps(request)).hexdigest()]
+            if not self.checkTransaction(request, originPublicKey):
+                print "Transaction is not valid."                
+                return False
+            self.parentPeer.writeRequest(request, originPublicKey, digitalSignature, timestamp, requestHash)
+            self.spreadRequest(requestHash)
+        if request[0] == 'REQUEST_FRIENDS':
+            self.sendFriends(clientSocket)
+        if request[0] == 'FRIENDS_LIST':
+            self.parentPeer.addFriends(request[1])
+        if request[0] == 'AWARE_REQUEST':
+            if not self.parentPeer.entries.get(request[1]):
+                self.askForEntry(request[1], clientSocket)
+        if request[0] == 'ENTRY_REQUEST':
+            self.respondSendingEntry(clientSocket, request[1])
         return
+        
+    def askForEntry(self, clientSocket, dataHash):
+        textPublicKey = self.publicKey.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        signer = self.privateKey.signer(padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
+        signer.update(json.dumps(['ENTRY_REQUEST', dataHash]))
+        digitalSignature = signer.finalize()
+        reqHash = hashlib.sha512(json.dumps(['ENTRY_REQUEST', dataHash])).hexdigest()        
+        clientSocket.send(json.dumps(['ENTRY_REQUEST', dataHash, time.time(), textPublicKey, digitalSignature, reqHash]))
+        return True
+        
+    def respondSendingEntry(self, clientSocket, dataHash):
+        request = self.parentPeer.entryList.get(dataHash)
+        if not request:
+            return False
+        self.parentPeer.sendRequestOnSocket(request, clientSocket)
+        return True
 
     def run(self):
         while(True):
             self.serverSocket.listen(10)
             clientSocket, connection = self.serverSocket.accept()
-            request = json.loads(clientSocket.recv(2048))
-            if request[0] == "REQUEST_FRIENDS":
-                self.sendFriends(clientSocket)
-            if request[0] == "PAY":
-                self.secureRequest(request)
+            data = json.loads(clientSocket.recv(2048))
+            print data
+            request = data[:-4]
+            originPublicKey = serialization.load_pem_public_key(str(data[-4]), backend=default_backend())
+            digitalSignature = data[-3]
+            timestamp = data[-2]
+            requestHash = data[-1]
+            if not self.checkRequest(request, originPublicKey, digitalSignature, timestamp, requestHash):
+                return False
+            self.processRequest(clientSocket, request, originPublicKey, digitalSignature, timestamp, requestHash)
         return True      
 
-class Request:
-    requestType = None
+class Entry:
+    reqHash = None
     timestamp = None
-    amount = None
-    origin = None
-    target = None
+    request = None
     
-    def __init__(self, rT, a, o, t):
-        self.requestType = rT
+    def __init__(self, request, timestamp):
         self.timestamp = time.time()
-        self.amount = a
-        self.origin = o
-        self.target = t
+        self.request = request
+        self.reqHash = hashlib.sha512(json.dumps([timestamp, request]))
         return
